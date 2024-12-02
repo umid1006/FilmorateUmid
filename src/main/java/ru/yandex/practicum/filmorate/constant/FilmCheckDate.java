33c3080c81c0df54d5291dc6f6c2ad7f907d751a
User Umid <umidjoninagamov@yandex.ru>
2024-12-02 06:06:05 +0500

    First commit

--- src/main/java/ru/yandex/practicum/filmorate/constant/FilmCheckDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/constant/FilmCheckDate.java
@@ -5,5 +5,5 @@ package ru.yandex.practicum.filmorate.constant;
  */
 public final class FilmCheckDate {
     public static final String OLDEST_DATE = "1895-12-28";
-    public final static String OLDEST_DATE_ERROR_MESSAGE = "Data must be after " + OLDEST_DATE;
+    public static final String OLDEST_DATE_ERROR_MESSAGE = "Data must be after " + OLDEST_DATE;
 }
